e246e4f998837b9d93e1701707cf35669ae3c3fc
caiyexiang <caiyexiang1@live.com>
2018-06-09 20:53:05 +0800

    啊啊啊啊

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,6 @@ const app = getApp()
 Page({
   data: {
     hasList:true, //TODO:在列表为空的时候将hasList=false
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     jarlist:[],
   },
   //事件处理函数
@@ -21,53 +18,7 @@ Page({
       url: '../help/help'
     })
   },
-  onLoad: function () {
-    wx.showNavigationBarLoading()
-    app.tokenCallback = token => {
-      wx.request({
-        url: 'https://tonylifepix.cn/api/item/list',
-        success: res => {
-          console.log(res.data)
-          this.setData({
-            jarlist: res.data.data.all_item,
-          })
-          wx.hideNavigationBarLoading()
-        },
-        data: {
-          'token': app.globalData.token
-        },
-        method: 'GET'
-      })
-    }
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
   onShow:function(e){
-    console.log('onShow页面更新')
     if(app.globalData.token.length!=0){
       wx.showNavigationBarLoading()
       wx.request({
@@ -84,6 +35,11 @@ Page({
         },
         method: 'GET'
       })
+    }else{
+      app.tokenCallback = token => {
+        wx.startPullDownRefresh()
+        wx.hideNavigationBarLoading()
+      }
     }
   },
   getUserInfo: function (e) {
